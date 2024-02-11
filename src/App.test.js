b40675cbdf8895920d85f281a4e708e4b8ecedb1
Tanaystudent <133126644+Tanaystudent@users.noreply.github.com>
2024-02-12 00:37:42 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
 describe('App component', () => {
