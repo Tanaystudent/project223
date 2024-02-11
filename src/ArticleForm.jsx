8120d621d811681a81e6b8821209d75164e50d71
Tanaystudent <133126644+Tanaystudent@users.noreply.github.com>
2024-02-12 00:38:40 +0530

    ArticleForm.jsx

--- src/ArticleForm.jsx
+++ src/ArticleForm.jsx
@@ -10,32 +10,36 @@ function ArticleForm() {
   return (
     <div className="article-form">
       <h2>What do you want to share?</h2>
-      <label htmlFor="Title">Title</label>
-      <input
-        type="text"
-        placeholder="Enter a descriptive title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <label htmlFor="Abstract">Abstract</label>
-      <textarea
-        value={abstract}
-        onChange={(e) => setAbstract(e.target.value)}
-        rows={3} 
-      />
-       <label htmlFor="Article Text">Article Text</label>
-      <textarea
-        value={articletext}
-        onChange={(e) => setArticletext(e.target.value)}
-        rows={7} 
-      />
-      <label htmlFor="Tags">Tags</label>
-      <input
-        type="text"
-        placeholder="Please add up to 3 tags"
-        value={tags}
-        onChange={(e) => setTags(e.target.value)}
-      />
+      <label htmlFor="articleTitle">Title</label>
+<input
+  id="articleTitle"
+  type="text"
+  placeholder="Enter a descriptive title"
+  value={title}
+  onChange={(e) => setTitle(e.target.value)}
+/>
+<label htmlFor="abstract">Abstract</label>
+<textarea
+  id="abstract"
+  value={abstract}
+  onChange={(e) => setAbstract(e.target.value)}
+  rows={3} 
+/>
+<label htmlFor="articleText">Article Text</label>
+<textarea
+  id="articleText"
+  value={articletext}
+  onChange={(e) => setArticletext(e.target.value)}
+  rows={7} 
+/>
+<label htmlFor="articleTags">Tags</label>
+<input
+  id="articleTags"
+  type="text"
+  placeholder="Please add up to 3 tags"
+  value={tags}
+  onChange={(e) => setTags(e.target.value)}
+/>
       <button>Post</button>
     </div>
   );
